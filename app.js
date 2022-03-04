802b4d55359a0d4cf0695dc6c693b921cdd42fde
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-03-04 16:34:37 -0300

    subo repositorio completo de aprendizajes de Js

---
+++ app.js
@@ -0,0 +1,63 @@
+let suma
+console.log(suma);
+
+suma = 10 + 15
+console.log(suma);
+
+const resta = 35 - 5
+console.log(resta);
+
+const texto = 'María Cecilia'
+const texto2 = ' Luna'
+console.log(texto + texto2);
+
+const booleanoTrue = true
+const booleanoFalse = false;
+
+const objeto = {
+  nombre: 'Tomás Gabriel',
+  apellido: 'Luna',
+  edad: 22,
+  hobbie: 'MMA',
+}
+console.log(objeto.apellido);
+
+const nulo = null;
+
+const indefinido = undefined;
+
+let sinDefinir
+console.log(sinDefinir);
+
+const diez = 10
+const dos = 2
+const res = diez - dos
+console.log(res)
+
+const res2 = diez + dos
+console.log(res2);
+
+const res3 = diez * dos
+console.log(res3);
+
+const res4 = diez / dos
+console.log(res4);
+
+const res5 = diez % dos
+console.log(res5);
+//
+
+let numero = 100
+numero ++
+console.log(numero);
+
+let numero2 = 1000
+numero2 --
+console.log(numero2);
+//
+
+let booleano = true
+console.log (!booleano);
+
+let booleano2 = false
+console.log(!booleano2);
